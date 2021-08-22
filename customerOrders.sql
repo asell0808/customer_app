2b8bf799a95f7f9f9b78862f1f00b17d01ccefe0
Anthony Sellers <asell0808@yahoo.com>
2021-08-21 23:59:58 -0700

    Update customerOrders.sql

--- customerOrders.sql
+++ customerOrders.sql
@@ -3,6 +3,7 @@
 
 CREATE DATABASE customer_app;
 
+-- Customers table 
 CREATE TABLE customers(
     id INT AUTO_INCREMENT PRIMARY KEY,
     first_name VARCHAR(100),
@@ -10,6 +11,7 @@ CREATE TABLE customers(
     email VARCHAR(100)
 );
 
+-- Orders table
 CREATE TABLE orders(
     id INT AUTO_INCREMENT PRIMARY KEY,
     order_date DATE,
@@ -18,6 +20,7 @@ CREATE TABLE orders(
     FOREIGN KEY(customer_id) REFERENCES customers(id)
 );
 
+-- Customer table values
 INSERT INTO customers (first_name, last_name, email) 
 VALUES ('Boy', 'George', 'george@gmail.com'),
        ('George', 'Michael', 'gm@gmail.com'),
@@ -25,12 +28,14 @@ VALUES ('Boy', 'George', 'george@gmail.com'),
        ('Blue', 'Steele', 'blue@gmail.com'),
        ('Bette', 'Davis', 'bette@aol.com');
        
+-- Order table values     
 INSERT INTO orders (order_date, amount, customer_id)
 VALUES ('2016/02/10', 99.99, 1),
        ('2017/11/11', 35.50, 1),
        ('2014/12/12', 800.67, 2),
        ('2015/01/03', 12.50, 2),
        ('1999/04/11', 450.25, 5);
-       
+
+-- Trying to run this will show how the foriegn keys are working because this wont be added to the table.
 INSERT INTO orders (order_date, amount, customer_id)
 VALUES ('2016/06/06', 33.67, 98);
